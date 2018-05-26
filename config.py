43accd65f1485ffbe41726a5626d4fcc05589bfc
wertas <wertas15@gmail.com>
2018-05-26 18:53:45 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,5 @@
 # -*- coding: utf-8 -*-
 # Этот токен невалидный, можете даже не пробовать :)
 import os
-from boto.s3.connection import S3Connection
-token = S3Connection(os.environ['API_ID'], os.environ['API_KEY'])
-print('lalala=',token)
-print(os.environ['API_KEY'])
-print(os.environ['API_ID'])
+token = os.environ['API_KEY']
+
