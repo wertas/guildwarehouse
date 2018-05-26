df46d3f4caf589792cc03775114ad6fb62df5e6f
wertas <wertas15@gmail.com>
2018-05-26 18:27:55 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,5 @@
 # Этот токен невалидный, можете даже не пробовать :)
 import os
 from boto.s3.connection import S3Connection
-token = S3Connection(os.environ['CWGWbotAPIKEY'])
+token = S3Connection(os.environ['API_ID'], os.environ['API_KEY'])
+print('lalala=',token)
