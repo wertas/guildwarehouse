410f5c1f6c2e38ea92a793711a684ed48a367097
wertas <wertas15@gmail.com>
2018-05-26 18:40:03 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,3 +4,5 @@ import os
 from boto.s3.connection import S3Connection
 token = S3Connection(os.environ['API_ID'], os.environ['API_KEY'])
 print('lalala=',token)
+print(os.environ['API_KEY'])
+print(os.environ['API_ID'])
